e9414dce70f0a940077b5e81ea7f4c0a0860d786
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-12-13 17:35:17 -0500

    add Option class, attachments, and clean up Post.get_absolute_url

--- wordpress/models.py
+++ wordpress/models.py
@@ -59,10 +59,10 @@ class WordPressModel(models.Model):
         except model.DoesNotExist:
             pass
         
-    def save(self, override=False):
+    def save(self, override=False, **kwargs):
         if READ_ONLY and not override:
             raise WordPressException, "object is read-only"
-        super(WordPressModel, self).save()
+        super(WordPressModel, self).save(**kwargs)
         
     def delete(self, override=False):
         if READ_ONLY and not override:
@@ -72,6 +72,30 @@ class WordPressModel(models.Model):
 #
 # WordPress models
 #
+
+class OptionManager(models.Manager):
+    def get_value(self, name):
+        try:
+            o = Option.objects.get(name=name)
+            return o.value
+        except Option.DoesNotExist:
+            pass
+
+class Option(WordPressModel):
+    objects = OptionManager()
+    
+    id = models.IntegerField(db_column='option_id', primary_key=True)
+    blog_id = models.IntegerField()
+    name = models.CharField(max_length=64, db_column='option_name')
+    value = models.TextField(db_column='option_value')
+    autoload = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = '%s_options' % TABLE_PREFIX
+        ordering = ["name"]
+    
+    def __unicode__(self):
+        return u"%s: %s" % (self.name, self.value)
 
 class User(WordPressModel):
     """
@@ -221,30 +245,27 @@ class Post(WordPressModel):
             taxonomy = "category"
             self.category_cache = self._get_terms(taxonomy)
         return self.category_cache
-        
-    def get_absolute_url(self):
-        year = self.post_date.year
-        month = "%02i" % self.post_date.month
-        day = "%02i" % self.post_date.day
-        slug = self.slug
-        return reverse('wp_object_detail', args=(year, month, day, slug))
-        
-    """   
+    
+    def attachments(self):
+        for post in Post.objects.filter(post_type='attachment', parent=self):
+            yield {
+                'id': post.id,
+                'slug': post.slug,
+                'timestamp': post.post_date,
+                'description': post.content,
+                'title': post.title,
+                'guid': post.guid,
+                'mimetype': post.mime_type,
+            }
+    
     @models.permalink
     def get_absolute_url(self):
-        params = {
-            "year": self.post_date.year,
-            "month": self.post_date.month,
-            "day": self.post_date.day,
-            "slug": self.slug,
-        }
-        return ('wp_object_detail', (), params)
-    """
-    
-    """
-    def parent(self):
-        return self._get_object(Post, self.parent_id)
-    """ 
+        return reverse('wp_object_detail', args=(
+            self.post_date.year,
+            "%02i" % self.post_date.month,
+            "%02i" % self.post_date.day,
+            self.slug
+        ))
      
     def tags(self):
         print self.get_absolute_url()
